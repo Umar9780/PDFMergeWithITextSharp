c3f3da208950e73255751cadc57631099d6118b5
Umar <umar@gmail.com>
2024-05-10 21:53:54 +0500

    Init

---
+++ MergePDF/PdfMerger.cs
@@ -0,0 +1,39 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergePDF
+{
+    public class PdfMerger
+    {
+        public void MergePdfFiles(string firstPdfPath, string secondPdfPath, string outputPdfPath)
+        {
+            using (FileStream outputStream = new FileStream(outputPdfPath, FileMode.Create))
+            {
+                Document document = new Document();
+                PdfCopy pdfCopy = new PdfCopy(document, outputStream);
+                document.Open();
+
+                AddPdfToMerge(pdfCopy, firstPdfPath);
+                AddPdfToMerge(pdfCopy, secondPdfPath);
+
+                document.Close();
+            }
+        }
+
+        private void AddPdfToMerge(PdfCopy pdfCopy, string pdfFilePath)
+        {
+            PdfReader pdfReader = new PdfReader(pdfFilePath);
+            for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+            {
+                PdfImportedPage importedPage = pdfCopy.GetImportedPage(pdfReader, page);
+                pdfCopy.AddPage(importedPage);
+            }
+            pdfReader.Close();
+        }
+    }
+}
---
+++ MergePDF/Program.cs
@@ -0,0 +1,16 @@
+﻿using MergePDF;
+
+string firstPdfPath = "C:\\Users\\Muham\\Downloads\\TourConfirmation.pdf";
+string secondPdfPath = "C:\\Users\\Muham\\Downloads\\Terms & Conditions.pdf";
+
+// Specify the output path for the merged PDF
+string outputPdfPath = "C:\\Users\\Muham\\Downloads\\outputMergedPdf.pdf";
+
+// Create an instance of the PdfMerger class
+PdfMerger pdfMerger = new PdfMerger();
+
+// Call the MergePdfFiles method to merge the PDF files
+pdfMerger.MergePdfFiles(firstPdfPath, secondPdfPath, outputPdfPath);
+
+// Output a message indicating the merge process is complete
+Console.WriteLine("PDF files merged successfully!");
